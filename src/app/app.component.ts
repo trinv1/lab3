2a60c5de07013bc9e3bbd5d39bd9ce612c781208
unknown <trinityvillaruel@gmail.com>
2024-02-21 11:22:13 +0000

    lab3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
 
 @Component({
-  selector: 'app-root',
+  selector:'root-directive',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AComponent, BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title:string = 'my-first-app';
+  name:string = "Trin";
+  age:number = 20;
 }
---
+++ src/app/b/b.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-b',
+  standalone: true,
+  imports: [],
+  templateUrl: './b.component.html',
+  styleUrl: './b.component.css'
+})
+export class BComponent {
+
+}
